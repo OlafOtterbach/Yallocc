ff2c42d12e86b01b2750259b41cef926f5179af7
Olaf <olaf.otterbach@gmx.de>
2015-04-04 08:53:17 +0200

    Überarbeiteter Parsermechanismus.

--- ParserLib/Parser.cs
+++ ParserLib/Parser.cs
@@ -4,65 +4,75 @@ using LexSharp;
 
 namespace ParserLib
 {
+   internal class TransistionStackElem
+   {
+      private TransistionStackElem _parentContext;
+
+      public TransistionStackElem(Transition transition, TransistionStackElem parentContext)
+      {
+         _parentContext = parentContext;
+         Transition = transition;
+      }
+
+      public Transition Transition { get; private set; }
+
+      public IEnumerable<TransistionStackElem> GetSuccessors()
+      {
+         var successors = Transition.Successors.Any()
+            ? Transition.Successors.Select( t => new TransistionStackElem(t, _parentContext))
+            : _parentContext == null
+               ? _parentContext.GetSuccessors()
+               : new List<TransistionStackElem>();
+         return successors;
+      }
+   }
+
+
    public class Parser<T>
    {
-      Stack<Transition> _path;
+      Stack<TransistionStackElem> _path;
 
       public Parser()
       {
-         _path = new Stack<Transition>();
+         _path = new Stack<TransistionStackElem>();
       }
 
-      public bool Parse(Transition startTransition, List<Token<T>> sequence, int index)
+
+      public void ParseTokens(IEnumerable<Token<T>> sequence)
       {
-         _path.Push(startTransition);
+//         var parseResult = sequence.Select( t => Lookahead(t)).TakeWhile(r => r.Success);
+         //ParseResult result = new ParseResult();
+         //foreach(var token in sequence)
+         //{
+         //   var res = Lookahead(token);
+         //   if(res != ParseResultType.Success)
+         //   {
+         //      result.ResultType = res;
+         //      result.Token = token;
+         //      break;
+         //   }
+         //}
+      }
+
+      public void LookAhead(T tokenType)
+      {
          while(_path.Count > 0)
          {
-            if (index >= sequence.Count)
+            var elem = _path.Pop();
+            if( (elem.Transition is TokenTypeTransition<T>) && ((elem.Transition as TokenTypeTransition<T>).TokenType.Equals(tokenType))
             {
-               return false;
+               // Hier noch Ergebnisrückgabe
+               return;
             }
-
-            var trans = _path.Pop();
-            if(trans is SyntaxNodeTransition<T>)
+            else if (elem.Transition is GrammarTransition)
             {
-               var syntaxNode = trans as SyntaxNode<T>;
-               if(syntaxNode.TokenType.Equals(sequence[index].Type))
-               {
-                  _path.Clear();
-                  index++;
-                  if(index == sequence.Count)
-                  {
-                     return true;
-                  }
-               }
-               else
-               {
-                  trans = new Transition(); // Dummytransition
-               }
+               _path.Push(new TransistionStackElem((elem.Transition as GrammarTransition).Grammar.StartTransition, elem));
             }
-            else if( trans is GrammarTransition)
+            else
             {
-               var grammerTransition = trans as GrammarTransition;
-               Parse(grammerTransition.Grammar.StartTransition, sequence, index);
-            }
-            trans.Successors.ToList().ForEach(x => _path.Push(x));
-         }
-         return true;
-      }
-
-      public void Lookahead(Transition start, T tokenType)
-      {
-         start.Successors.ToList().ForEach(t => _path.Push(t));
-         while (_path.Count > 0)
-         {
-            var transition = _path.Pop();
-            if((transition is SyntaxNodeTransition<T>))
-            {
-               var token = (transition as SyntaxNodeTransition<T>). 
+               elem.GetSuccessors().ToList().ForEach(e => _path.Push(e));
             }
          }
-
       }
    }
 }
---
+++ ParserLib/TokenTypeTransition.cs
@@ -0,0 +1,12 @@
+﻿namespace ParserLib
+{
+   internal class TokenTypeTransition<T> : Transition
+   {
+      public TokenTypeTransition( T tokenType )
+      {
+         TokenType = tokenType;
+      }
+
+      public T TokenType { get; private set; }
+   }
+}
