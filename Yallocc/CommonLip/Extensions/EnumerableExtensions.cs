cee6064a5b0ab08bf957f753b5e4f12172ae152c
otterbach <olaf.otterbach@gmx.de>
2016-05-18 06:15:48 +0200

    Implementing new extensions and renaming project.

--- Yallocc/CommonLip/Extensions/EnumerableExtensions.cs
+++ Yallocc/CommonLip/Extensions/EnumerableExtensions.cs
@@ -6,8 +6,36 @@ namespace Yallocc.CommonLib
 {
    public static class EnumerableExtensions
    {
+      public static IEnumerable<T> ToEnumerabele<T>(this T element)
+      {
+         if (element == null) throw new ArgumentNullException("element");
+         yield return element;
+      }
+
+      public static IEnumerable<IEnumerable<T>> SplitHeaderAndTail<T>(this IEnumerable<T> elems)
+      {
+         if (elems == null) throw new ArgumentNullException("elems");
+         var iter = elems.GetEnumerator();
+         if (iter.MoveNext())
+         {
+            var header = iter.Current;
+            yield return header.ToEnumerabele();
+         }
+         var tail = SeparateTail(iter);
+         yield return tail;
+      }
+
+      private static IEnumerable<T> SeparateTail<T>(this IEnumerator<T> iter)
+      {
+         while (iter.MoveNext())
+         {
+            yield return iter.Current;
+         }
+      }
+
       public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> elems, Func<T, bool> separator)
       {
+         if (elems == null) throw new ArgumentNullException("elems");
          var iter = elems.GetEnumerator();
          while (iter.MoveNext())
          {
